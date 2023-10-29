84d43b25714a2b6c7715ee112398e062c362f47a
KaiYao <lumky11.22@ichat.sp.edu.sg>
2023-10-29 16:54:54 +0800

    New bmi version

--- bmi.py
+++ bmi.py
@@ -8,10 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi<18.5:
         print("Under Weight")
+        return -1
     elif bmi<=25.0:
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 
 calculate_bmi(weight=57,height=1.73)
