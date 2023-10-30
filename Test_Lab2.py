51a7be6c6d2820881794bf74f39a68f60d87d146
KaiYao <lumky11.22@ichat.sp.edu.sg>
2023-10-30 21:42:04 +0800

    V3.0 latest

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -2,23 +2,23 @@ import Lab2
 
 def test_find_min_max():
     num_list=[23,30,47,60,67]
-    list_result=[23,67]
+    result=[]
 
     result=Lab2.calc_min_max_temperature(num_list)
-    assert (result==list_result)
+    assert (result==(23,67))
 
 
 def test_calc_average():
     num_list = [23, 30, 47, 60, 67]
-    list_result=45.4
+    result=[]
 
     result=Lab2.calc_average_temperature(num_list)
-    assert (result==list_result)
+    assert (result==45.4)
 
 
 def test_median_temperature():
     num_list = [23, 30, 47, 60, 67]
-    list_result=47
+    result=[]
 
     result=Lab2.calc_median_temp(num_list)
-    assert result==list_result
+    assert result==47
